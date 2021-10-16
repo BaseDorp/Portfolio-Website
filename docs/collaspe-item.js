aca06e2bad4aa12a968262a1ea3eda0d50957223
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2021-10-15 20:24:42 -0500

    Added collapsible project containers

---
+++ docs/collaspe-item.js
@@ -0,0 +1,23 @@
+// get all the drop down bars
+let projects = document.querySelectorAll(".project-bar");
+
+// add an on-click event to each project-bar
+for (let item of projects) {
+    item.addEventListener("click", collapsibleClicked);
+}
+
+function collapsibleClicked() {
+    // this.classList.toggle("active"); // TODO idk what this does but i dont want it
+    let content = this.nextElementSibling;
+
+    // checks if the content is collapsed or not
+    if (content.style.maxHeight) {
+        // closes
+        content.style.maxHeight = null;
+        this.querySelector("img").style.transform = "rotate(180deg)";
+    } else {
+        // opens
+        content.style.maxHeight = content.scrollHeight + "px";
+        this.querySelector("img").style.transform = "rotate(-90deg)";
+    }
+}
