789fc8687a8423013f1565224c978b0501a7da59
Sam Hirsch <samhirsch.dev@gmail.com>
2021-10-25 14:33:38 -0500

    Fixed Slideshow

--- docs/slideshow.js
+++ docs/slideshow.js
@@ -1,71 +1,34 @@
 /*
-    derived from https://www.w3schools.com/howto/howto_js_slideshow.asp
+    Received help from colleague Jethro Schoppenhorst
+    https://github.com/JSchoppe
 */
 
-// get all slideshows
-let slideshows = document.querySelectorAll(".slideshow");
-// go through each slideshow and see which slide is active
+document.querySelectorAll(".slideshow").forEach((slideshow)=>{
 
-const Slideshow = {
-    index: 0,
-    slides: null,
+    // Encapsulate state (this is unique for each slideshow).
+    let slideIndex = 0;
+    const slides = slideshow.querySelectorAll(".slide");
 
-}
-
-function nextSlide(slideshow) {
-    let slides = slideshow.querySelectorAll(".slide");
-    changeSlide(slideshow)
-
-
-    slideshow.currentslide = slides[currentslide++];
-
-    console.log(slideshow);
-}
-
-function prevSlide(div) {
-
-}
-
-
-
-
-
-
-
-let slideIndex = 1;
-changeSlide(slideIndex);
-
-function changeSlide(n) {
-    slideIndex += n;
-    let slides = document.querySelectorAll(".slide");
-
-    // loops the slideshow back around
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    if (slideIndex < 1) {
-        slideIndex = slides.length;
-    }
-
-    for (let ss of slideshows) {
-        let slides = document.querySelectorAll(".slide");
+    // Define behaviour when clicking.
+    const IncrementSlideIndex = (n) => {
+        // Update slide index and wrap.
+        slideIndex += n;
+        if (slideIndex > slides.length)
+            slideIndex = 1;
+        if (slideIndex < 1)
+            slideIndex = slides.length;
+        // Update styling.
         for (let s of slides) {
             // sets all slides to not visible
             s.style.display = "none";
         }
+        slides[slideIndex - 1].style.display = "block";
+    };
+
+    if (slides.length > 1) {
+    IncrementSlideIndex(1); // This automatically sets the first slide
+    // Bind click actions.
+    slideshow.querySelector(".prev").addEventListener("click", () => { IncrementSlideIndex(-1); });
+    slideshow.querySelector(".next").addEventListener("click", () => { IncrementSlideIndex(1); });
     }
-
-    // set current slide to visible
-    slides[slideIndex - 1].style.display = "block";
-}
-
-
-
-
-// TODO make next/prev arrows and caption snap to slide depending on size
-// function onPageLoaded(){
-//     document.querySelectorAll(".slideshow").forEach((slideshowDiv) => {
-//         slideshowDiv.querySelectorAll(".slide");
-//         // TODO
-//     };
-// }
+});
